d67629edeb9791a7dca5211698642fcbfbd556b1
sirrah23 <hseth93@gmail.com>
2018-04-01 21:30:58 -0400

    Getting progress for input anime now

--- extension_browser/src/scripts/index.js
+++ extension_browser/src/scripts/index.js
@@ -41,16 +41,35 @@ function appendURLToApp(text, link){
 function queryAnimeByTitle(title, access_token){
 	// Here we define our query as a multi-line string
 	// Storing it in a separate .graphql/.gql file is also possible
+	/*
+		let query = `
+		query{
+			User(name: "my-username"){
+				id
+			}
+		}
+	*/
+	//TODO: 
+	//1. Get userId
+	//2.Get MediaId 
+	//3. Feed into query
 	let query = `
-	query ($search: String) { # Define which variables will be used in the query (id)
-	  Media (search: $search) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
-	    id
-	    title {
-	      romaji
-	      english
-	      native
+	{
+		MediaList(mediaId: 98460, userId: 80196){
+	    progress,
+	    status,
+	    media{
+	      title {
+	        romaji
+	        english
+	        native
+	        userPreferred
+	      }
+	    },
+	    user{
+	      name
 	    }
-	  }
+	  }  
 	}
 	`;
 
@@ -81,13 +100,14 @@ function queryAnimeByTitle(title, access_token){
 
 	function handleResponse(response) {
 	    return response.json().then(function (json) {
+	    	console.log(response)
+	    	console.log(json)
 	        return response.ok ? json : Promise.reject(json);
 	    });
 	}
 
 	function handleData(data) {
-	    console.log(data.data.Media.id);
-	    console.log(data.data.Media.title);
+		console.log(data)
 	}
 
 	function handleError(error) {
