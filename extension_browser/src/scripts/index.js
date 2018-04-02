354e67475435c0cbd7bb5ca284a36c0fcb158882
sirrah23 <hseth93@gmail.com>
2018-04-02 01:39:19 -0400

    Abstracted GraphQL query code and added comments

--- extension_browser/src/scripts/index.js
+++ extension_browser/src/scripts/index.js
@@ -1,5 +1,13 @@
+// Node that contains the main app information
 const app_node = document.getElementById("app");
 
+/**
+ * Make a request to the server for either a link or 
+ * an access token which helps us determine what to
+ * display to the user:
+ * - A  login link to the Anilist API auth website
+ * - An anime title for the most recently seen anime
+ */
 fetch("http://localhost:5000/get_auth_url_or_token", {mode: "cors",})
 	.then(res => res.json())
 	.then(res_json => {
@@ -14,21 +22,36 @@ fetch("http://localhost:5000/get_auth_url_or_token", {mode: "cors",})
 			let text = computePopupTextContent("CRTitle", "No anime yet!");
 			let token = res_json.data;
             appendTextToApp(text);
-            queryAnimeByTitle(text, token);
+            //queryAnimeProgress(<media_id>, <user_id>, token);
 		}
 	});
 
+/**
+ * Attempt to grab a key from localStorage and if it does
+ * not exist return a default value.
+ * @param {string} key The key to try and locate in browser local storage
+ * @param {string} default_value Return if key is not in browser local storage
+ */
 function computePopupTextContent(key, default_value){
 	const text = localStorage.getItem(key)
 	if(!text) return default_value
 	return text
 }
 
+/**
+ * Append a text node ot the div with the app id
+ * @param {string} text The text that popualtes the text node
+ */
 function appendTextToApp(text){
     const token_msg_node = document.createTextNode(text);
     app_node.appendChild(token_msg_node);
 }
 
+/**
+ * Append a <a> tag to the div with the app id
+ * @param {string} text The text for the link
+ * @param {string} link The actual url you get directed to upon clicking the link
+ */
 function appendURLToApp(text, link){
     const auth_url_node = document.createElement("a");
     const auth_url_text_node = document.createTextNode(text);
@@ -38,80 +61,74 @@ function appendURLToApp(text, link){
     app_node.appendChild(auth_url_node);
 }
 
-function queryAnimeByTitle(title, access_token){
-	// Here we define our query as a multi-line string
-	// Storing it in a separate .graphql/.gql file is also possible
-	/*
-		let query = `
-		query{
-			User(name: "my-username"){
-				id
-			}
-		}
-	*/
-	//TODO: 
-	//1. Get userId
-	//2.Get MediaId 
-	//3. Feed into query
-	let query = `
-	{
-		MediaList(mediaId: 98460, userId: 80196){
-	    progress,
-	    status,
-	    media{
-	      title {
-	        romaji
-	        english
-	        native
-	        userPreferred
-	      }
-	    },
-	    user{
-	      name
-	    }
-	  }  
-	}
-	`;
+/**
+ * Queries the Anilist GraphQL API
+ * @param {string} query The GraphQL query that you want to run
+ * @param {Object} variables The values for the variables in your query
+ * @param {string} access_token OAuth token to access the API
+ * @return {Promise} Contains JSON response from API
+ */
+function queryAnilistAPI(query, variables, access_token){
+
+	let url = 'https://graphql.anilist.co';
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + access_token
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })
+    };
+
+	return fetch(url, options)
+        .then(res => res.json());
+}
+
+/**
+ * Given the id for an anime and a user query the
+ * Anilist API to get the progress that the user 
+ * has made so far on the anime.
+ * @param {number} media_id The id for the anime that you want progress information
+ * @param {number} user_id The id for the user whose progress information you want
+ * @param {string} access_token OAuth token for accessing the api
+ */
+function queryAnimeProgress(media_id, user_id, access_token){
+	const query =`
+    query($mediaId: Int, $userId: Int){
+        MediaList(mediaId: $mediaId, userId: $userId){
+            progress,
+            status,
+            media{
+                title {
+                    romaji
+                    english
+                    native
+                    userPreferred
+                }
+            }
+        }  
+    }`;
 
 	// Define our query variables and values that will be used in the query request
-	let variables = {
-	    "search": title
+	const variables = {
+	    "mediaId": media_id,
+	    "userId": user_id
 	};
 
-	// Define the config we'll need for our Api request
-	let url = 'https://graphql.anilist.co',
-	    options = {
-	        method: 'POST',
-	        headers: {
-	            'Content-Type': 'application/json',
-	            'Accept': 'application/json',
-	            'Authorization': 'Bearer ' + access_token
-	        },
-	        body: JSON.stringify({
-	            query: query,
-	            variables: variables
-	        })
-	    };
-
-	// Make the HTTP Api request
-	fetch(url, options).then(handleResponse)
-	                   .then(handleData)
-	                   .catch(handleError);
-
-	function handleResponse(response) {
-	    return response.json().then(function (json) {
-	    	console.log(response)
-	    	console.log(json)
-	        return response.ok ? json : Promise.reject(json);
-	    });
-	}
+    queryAnilistAPI(query, variables, access_token)
+        .then(handleData)
+        .catch(handleError);
 
 	function handleData(data) {
 		console.log(data)
 	}
 
 	function handleError(error) {
-	    alert('Error, check console');
 	    console.log(error);
 	}
 }
+
